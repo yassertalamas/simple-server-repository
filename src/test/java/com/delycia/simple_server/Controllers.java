9e5859ef563cff232fad189748f8b16889348c53
yassertalamas <yasser.talamas@fxreplay.com>
2026-02-12 19:30:15 -0400

    Create Controllers.java

---
+++ src/test/java/com/delycia/simple_server/Controllers.java
@@ -0,0 +1,15 @@
+package com.delycia.simple_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Controllers {
+    
+    @GetMapping("/saludo");
+    public String oneEndpoint() {
+        return "Hello World! /api/v1/saludo";
+    }
+}
