62abb607affd1373e08bd0aefa48818bfe05f65e
ytalamas <ytalamas.suarez@gmail.com>
2026-02-13 21:29:50 +0000

    Integrating with environment variables

--- src/main/java/com/delycia/simple_server/Controllers.java
+++ src/main/java/com/delycia/simple_server/Controllers.java
@@ -1,5 +1,6 @@
 package com.delycia.simple_server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class Controllers {
 
+    @Value("${DB_PASSWORD}")
+    public String valorcito;
     @GetMapping("/saludo")
     public String oneEndpoint() {
-        return "Hello World! /api/v1/saludo";
+        return "Hello World! /api/v1/saludoooooo " + valorcito;
     }
 }
 
